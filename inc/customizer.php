37ede826e0badd997e62892e6638b9b5e21bc6b8
Difa Winanda Adliya <difwinda@gmail.com>
2024-06-04 02:25:15 +0700

    initiate Header Options, the purpose is for the right menu options

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,8 +5,30 @@
  * @package Rancak
  */
 
- function theme_customizer_function(){
+ function theme_customizer_function($wp_customize){
+    $wp_customize->add_panel('header_option_panel', array(
+        'title' => 'Header Options',
+        'priority' => 30,
+        'capability' => 'edit_theme_options',
+    ));
 
+    //Menu Settings
+    $wp_customize->add_section('menu_options', array(
+        'title' => 'Menu',
+        'capability' => 'edit_theme_options',
+        'panel' => 'header_option_panel',
+        'priority' => 10,
+    ));
+
+    $wp_customize->add_setting('menu_settings', array(
+        'capability' => 'edit_theme_options',
+        'priority' => 1,
+    ));
+    $wp_customize->add_control('menu_settings', array(
+        'type' => 'hidden',
+        'label' => 'Menu',
+        'section' => 'menu_options',
+    ));
  }
 
  add_action('customize_register', 'theme_customizer_function');

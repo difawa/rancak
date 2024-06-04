83e2ec742c3b59f35533651ee98e994a103fbd81
Difa Winanda Adliya <difwinda@gmail.com>
2024-06-05 05:19:43 +0700

    repair navbar header

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -9,6 +9,5 @@ wp_nav_menu( array(
     'theme_location' => 'header-menu',
     'container' => 'nav',
     'container_class' => 'col-lg-7 d-lg-block none',
-    'container_id' => 'menu-header-container',
-    'menu_class' => 'flex-row position-relative'
+    'menu_class' => 'flex-row position-relative navbar'
 ));

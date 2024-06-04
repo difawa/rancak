9eb5ca9931c9d8f7c8cdd422638516f1d2c9f8b1
Difa Winanda Adliya <difwinda@gmail.com>
2024-06-04 16:02:48 +0700

    -

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -10,5 +10,5 @@ wp_nav_menu( array(
     'container' => 'nav',
     'container_class' => 'col-lg-7 d-lg-block none',
     'container_id' => 'menu-header-container',
-    'menu_class' => 'flex-row relative'
+    'menu_class' => 'flex-row position-relative'
 ));
